40220603fc52adcec0f3237d2a08327df5e6d61c
Jin Kim <golbin@gmail.com>
2018-01-22 10:40:13 +0900

    Seperate constant values

---
+++ gcoin/config.py
@@ -0,0 +1,11 @@
+# for the first chain
+GENESIS_HASH = 'g'
+GENESIS_PROOF = 10
+
+# for mining rewards
+GENESIS_ACCOUNT_ID = '0'
+AMOUNT_OF_REWARD = 1
+
+# for proof of work
+DIFFICULTY = 1  # number of digits is difficulty
+VALID_DIGITS = '0' * DIFFICULTY
